fec58b8b1e9f81fc2ca2cfeb6f397c49f0eed6ac
MattLeone10 <mleone10@u.rochester.edu>
2021-07-11 15:58:55 -0400

    copied lab01

---
+++ inc/nav.php
@@ -0,0 +1,11 @@
+	<nav class="navigation">
+
+		<ul>
+			<li><a href="index.php">Home</a></li>
+			<li><a href="history.php">History</a></li>
+			<li><a href="parks.php">Parks</a></li>
+			<li><a href="geography.php">Geography</a></li>
+		</ul>
+
+	</nav>
+
